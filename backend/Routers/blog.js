7809e40108e02df03989278767facb34a4cb0fbe
mohammadashrafulislam <ixdell41@gmail.com>
2024-07-17 09:26:19 +0530

    added get  Blog api

--- backend/Routers/blog.js
+++ backend/Routers/blog.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { addBlog } from '../Controllers/BlogController';
+import { addBlog, getBlogs } from '../Controllers/BlogController';
 import { upload } from '../MiddleWare/multer';
 
 export const blogRouter = express.Router();
