cbaf77eca926cc47357eed151d81c16bd0af0bff
mohammadashrafulislam <ixdell41@gmail.com>
2024-07-26 10:26:09 +0530

    added controllor for add city and get city

---
+++ backend/Controllers/CityController.js
@@ -0,0 +1,27 @@
+
+import { CityModel } from "../Models/CityModel.js";
+import { cloudinary } from "../utils/cloudinary.js";
+
+// add Amenities controller:
+export const addCity = async(req, res)=>{
+    try{
+        const imageResult = await cloudinary.uploader.upload(req.file.path);
+        const city = new CityModel({name: req.body.name, image:imageResult.secure_url})
+        const savedCity = await city.save();
+        res.status(200).json(savedCity)
+    }
+    catch (e){
+        console.log(e.message)
+        res.status(500).json({message: "Internal Serval Error."})
+    }
+}
+// get Amenities controller:
+export const getCity = async (req, res) =>{
+    try{
+      const city = await CityModel.find();
+      res.status(200).json(city)
+    }catch (e) {
+        console.log(e.message);
+        res.status(500).json({ message: "Internal Server Error."});
+
+}}
