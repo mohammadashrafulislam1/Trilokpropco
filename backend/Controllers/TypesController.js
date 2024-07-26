57b0ae202f144cb2cf68c1ff591ab63bdd5d8418
mohammadashrafulislam <ixdell41@gmail.com>
2024-07-26 14:01:28 +0530

    conditional statement for types

--- backend/Controllers/TypesController.js
+++ backend/Controllers/TypesController.js
@@ -4,6 +4,12 @@ import { cloudinary } from "../utils/cloudinary.js";
 // add Types controller:
 export const addType = async(req, res)=>{
     try{
+        const { name } = req.body;
+        const existingProject = await TypesModel.findOne({ name });
+
+        if (existingProject) {
+            return res.status(400).json({ message: "Type already exists." });
+        }
         const imageResult = await cloudinary.uploader.upload(req.file.path);
         const type = new TypesModel({type: req.body.type, logo:imageResult.secure_url})
         const savedType = await type.save();
