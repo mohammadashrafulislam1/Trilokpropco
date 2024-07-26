937c85a87d6be2e2f7bbcffc9cf30ae05c499f30
mohammadashrafulislam <ixdell41@gmail.com>
2024-07-26 10:23:08 +0530

    city model

---
+++ backend/Models/CityModel.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+
+const citySchema = new mongoose.Schema({
+        name: { type: String, required: true },
+        image: { type: String, required: true }
+})
+
+export const CityModel = mongoose.model('city', citySchema);
