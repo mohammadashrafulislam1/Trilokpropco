75537970ff45b2b435831844204af612b09af320
mohammadashrafulislam <ixdell41@gmail.com>
2024-07-27 12:03:46 +0530

    add testimonial controller

---
+++ backend/Controllers/TestimonialController.js
@@ -0,0 +1,20 @@
+import { TestimonialModel } from "../Models/TestimonialModel";
+
+
+export const addTestimonial = async(req, res) =>{
+    try{
+        const imageResult = await cloudinary.uploader.upload(req.file.path);
+        const testimonial = new TestimonialModel({
+            name: req.body.name, 
+            des: req.body.des, 
+            details: req.body.details, 
+            rating: req.body.rating, 
+            image:imageResult.secure_url})
+        const savedTestimonial = await testimonial.save();
+        res.status(200).json(savedTestimonial)
+    }
+    catch (e){
+        console.log(e.message)
+        res.status(500).json({message: "Internal Serval Error."})
+    }
+}
