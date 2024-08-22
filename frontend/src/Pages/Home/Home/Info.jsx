a09789e05581e886ad085d8d07896bad64619eee
mohammadashrafulislam <ixdell41@gmail.com>
2024-08-22 16:29:50 +0600

    added info 04

--- frontend/src/Pages/Home/Home/Info.jsx
+++ frontend/src/Pages/Home/Home/Info.jsx
@@ -5,12 +5,30 @@
 
 const Info = () => {
     return (
-    <div>
+    <div className="flex gap-10 my-20 justify-center">
  
     <div className="bg-[#181818] text-center w-[200px] h-[200px] flex items-center justify-center flex-col">
-        <img src="https://i.ibb.co/Y8KMFDn/Mask-group-3.webp" alt="" />
+        <img src="https://i.ibb.co/Y8KMFDn/Mask-group-3.webp" alt="total properties" />
         <h3 className="text-[#e2e7ee] text-[38px] font-bold">60</h3>
         <p className="text-[#fff] text-[13px]">total properties</p>
+    </div>   
+
+    <div className="bg-[#181818] text-center w-[200px] h-[200px] flex items-center justify-center flex-col">
+        <img src="https://i.ibb.co/LrW9b6S/Mask-group-6.webp" alt="sold properties" />
+        <h3 className="text-[#e2e7ee] text-[38px] font-bold">60</h3>
+        <p className="text-[#fff] text-[13px]">sold properties</p>
+    </div>   
+
+    <div className="bg-[#181818] text-center w-[200px] h-[200px] flex items-center justify-center flex-col">
+        <img src="https://i.ibb.co/Pt39cv1/Mask-group-7.webp" alt="properties for sale" />
+        <h3 className="text-[#e2e7ee] text-[38px] font-bold">60</h3>
+        <p className="text-[#fff] text-[13px]">properties for sale</p>
+    </div>   
+
+    <div className="bg-[#181818] text-center w-[200px] h-[200px] flex items-center justify-center flex-col">
+        <img src="https://i.ibb.co/HBxYv2H/emoticon-happy.webp" alt="happy customers" />
+        <h3 className="text-[#e2e7ee] text-[38px] font-bold">60</h3>
+        <p className="text-[#fff] text-[13px]">happy customers</p>
     </div>       
     
     </div>
